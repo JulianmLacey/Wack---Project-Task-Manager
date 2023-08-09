d1aaaf5b9759c175b132925bd271074c8af52ff4
JulianmLacey <Julianmlacey@gmail.com>
2023-08-09 15:17:34 -0700

    Initial Commit

---
+++ server.js
@@ -0,0 +1,75 @@
+/*
+
+BrainStorming
+
+Slack - wack
+
+
+Chanel model
+    - users list
+    - ALL
+
+    DM
+    - user a, user b
+
+
+- Organization
+    - Channels
+        - message threads
+        - all users
+
+    - direct messages
+       - 2 users
+
+
+
+    - Users
+CHANNEL
+    -forum
+
+
+
+Slack/project Tracker - wack
+
+Organization
+    - Channels
+        - isPublic
+            - YES; ALL USERS
+            - NO; ONLY TASK USERS
+                - TASK ASSIGNED TO
+            - NO; ONLY INVITED USERS
+        
+        - message stuff
+            content, date, created by, etc
+
+    - Users
+        - ID
+        - TASK LIST
+        - EMAIL
+        - PASSWORD
+
+        - PROFILE 
+            - NAME
+            - PROFILE PIC?
+            - BIO
+            
+
+    - task board
+        - TASK LIST
+            - TASK
+                - NAME
+                - TASK ID
+                - TIMELINE (Time Left)
+                - STATUS
+                - PRIORITY
+                - ASSIGNED TO (USERIDs)
+                
+
+
+Faker npm package
+
+
+
+
+
+*/
