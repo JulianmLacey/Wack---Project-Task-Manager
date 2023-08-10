22adf9f5993da578f2a0e67ae2006fc65ae74ac5
JulianmLacey <Julianmlacey@gmail.com>
2023-08-10 10:31:45 -0700

    Added routes outline

---
+++ controllers/api/task-routes.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+const { Task } = require("../../models");
+
+//GET ALL TASKS
+
+//GET TASK BY USER
+
+//GET INDIVIDUAL TASK
+
+//CREATE TASKS
+
+//UPDATE TASK
+
+//DELETE TASK
---
+++ controllers/api/user-routes.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+const { User } = require("../../models");
+
+//POST SIGNUP - add user
+
+//post login - check user
+
+//post logout
+
+//put update user
+
+//delete user
+
+module.exports = router;
