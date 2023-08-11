99b3fb5c001b1fb6d439e41b0d106c64bdb3a650
Thomas Munzar <tmunzar92@gmail.com>
2023-08-11 11:43:13 -0700

    edited post req

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,54 +1,61 @@
 const router = require('express').Router();
 const { Comment } = require('../../models');
 
-router.get('/',async (req, res) => {
+router.get('/', async (req, res) => {
     // find all categories
     // be sure to include its associated Products
     try {
-      const comments = await Comment.findAll({
-        include: [
-            {
-              model: User,
-              attributes: ['name'],
-            },
-          ],
-      });
-      res.status(200).json(comments);
+        const comments = await Comment.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+            ],
+        });
+        res.status(200).json(comments);
     } catch (err) {
-      res.status(500).json(err);
+        res.status(500).json(err);
     }
-  });
-  
 
-  
-  router.post('/', async (req, res) => {
+});
+
+
+
+router.post('/', async (req, res) => {
     // create a new comment
-    try{
-      const newComment = await Comment.create({content, user_id: req.session.user_id, project_id});
-      res.status(200).json(newComment);
-    }catch (err){
-      res.status(500).json(err);
+    try {
+        const { content, project_id } = req.body;
+        const user_id = req.session.user_id;
+        const newComment = Comment.create({
+            content: content,
+            user_id: user_id,
+            project_id: project_id,
+        });
+        res.status(200).json(newComment);
+    } catch (err) {
+        res.status(500).json(err);
     }
-  });
-  
-  router.put('/:id',async (req, res) => {
+});
+
+router.put('/:id', async (req, res) => {
     // update a comment by its `id` value
-    try{
-      const comment = await Comment.update(req.body,{where:{id:req.params.id}}) 
-      res.status(200).json(comment);
-    }catch (err){
-      res.status(500).json(err);
+    try {
+        const comment = await Comment.update(req.body, { where: { id: req.params.id } })
+        res.status(200).json(comment);
+    } catch (err) {
+        res.status(500).json(err);
     }
-  });
-  
-  router.delete('/:id',async (req, res) => {
+});
+
+router.delete('/:id', async (req, res) => {
     // delete a comment by its `id` value
-    try{
-      const comment = await Comment.destroy({where:{id:req.params.id}})
-      res.status(200).json(comment);
-    }catch (err){
-      res.status(500).json(err);
+    try {
+        const comment = await Comment.destroy({ where: { id: req.params.id } })
+        res.status(200).json(comment);
+    } catch (err) {
+        res.status(500).json(err);
     }
-  });
-  
-  module.exports = router;
+});
+
+module.exports = router;
