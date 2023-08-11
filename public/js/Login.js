f3f1c62ad1789cfb74597b429b17513f7072f742
Jacob Kachmarek <jacob.kachmarek@gmail.com>
2023-08-11 13:12:49 -0700

    re added console log so i can change caps in file name

--- public/js/Login.js
+++ public/js/Login.js
@@ -18,6 +18,7 @@ const loginEventHandler = async (event) => {
 		if (response.ok) {
 			// If successful, redirect the browser to the profile page
 			document.location.replace('/projects');
+			console.log(response);
 			console.log("Login Successful");
 		} else {
 			alert(response.statusText);
