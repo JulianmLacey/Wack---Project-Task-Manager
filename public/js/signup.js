0cc20e5902631d9f5f8872d1976f26dbf7902f88
Jacob Kachmarek <jacob.kachmarek@gmail.com>
2023-08-12 15:08:38 -0700

    imported bootsrap and restructured our login and singup pages and associated js

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,42 +1,28 @@
-const signupUSER = document.getElementById("SignUp-submit");
-const getLoginPage = document.getElementById("GetLoginPage");
+const signUpForm = document.querySelector('#sign-up-form')
+const username = document.getElementById('username')
+const password = document.getElementById('password')
 
-const signUpEventHandler = async (event) => {
-	event.preventDefault();
 
-	const Iname = document.querySelector("#signup-Username").value.trim();
-	const Iemail = document.querySelector("#signup-Email").value.trim();
-	const Ipassword = document.querySelector("#signup-Password").value.trim();
-	const Iorganization = document.querySelector("#signup-orgID").value.trim();
+signUpForm.addEventListener('submit', async function (event) {
+	event.preventDefault()
 
-	console.log(Iname);
-	console.log(Iemail);
-	console.log(Ipassword);
-	console.log(Iorganization);
-
-	if (Iemail && Ipassword && Iname && Iorganization) {
-		// Send a POST request to the API endpoint
-		const response = await fetch("/USERsignup", {
-			method: "POST",
-			body: JSON.stringify({ name: Iname, email: Iemail, password: Ipassword, organization: Iorganization }),
-			headers: { "Content-Type": "application/json" },
-		});
-
-		if (response.ok) {
-			// If successful, redirect the browser to the profile page
-			document.location.replace("/login");
-			console.log(response);
-		} else {
-			alert(response.statusText);
-		}
+	const bodyObj = {
+		name: username.value,
+		email: email.value,
+		password: password.value
 	}
-};
 
-const getLoginPageHandler = async (event) => {
-	event.preventDefault();
-	console.log("Get Login Page");
-	document.location.replace("/login");
-};
+	const response = await fetch('/api/users', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(bodyObj)
+	})
 
-document.getElementById("SignUp-submit").addEventListener("click", signUpEventHandler);
-document.getElementById("GetLoginPage").addEventListener("submit", getLoginPageHandler);
+	if (response.ok) {
+		window.location.href = '/'
+	} else {
+		const json = await response.json()
+	}
+}) 
