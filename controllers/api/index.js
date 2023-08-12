9504f503320ba172f722937e6416a0a325058cf9
Jacob Kachmarek <jacob.kachmarek@gmail.com>
2023-08-12 13:55:25 -0700

    adding task routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,14 +2,13 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes');
 const projectRoutes = require('./project-routes')
 const commentRoutes = require('./comment-routes');
-// const projectRoutes = require('./project-routes');
-// const taskRoutes = require('./task-routes');
+const taskRoutes = require('./task-routes');
 
 
 router.use('/users', userRoutes);
 router.use('/comments', commentRoutes);
 router.use('/projects', projectRoutes);
-// router.use('./tasks', taskRoutes);
+router.use('/tasks', taskRoutes);
 
 
 module.exports = router;
