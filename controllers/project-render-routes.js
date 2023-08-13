c80e5f791fca6cf4811c387b5d04d43ad841cdf3
Jacob Kachmarek <jacob.kachmarek@gmail.com>
2023-08-13 10:07:31 -0700

    adding render routes for creating a project

--- controllers/project-render-routes.js
+++ controllers/project-render-routes.js
@@ -1,17 +1,16 @@
 const router = require('express').Router();
-const { Project, User } = require('../../models');
+const { Project, User } = require('../models');
 
 
 //post projects
 
 router.get('/projects', async (req, res) => {
     try {
-        const projects = await Project.findAll({
-            where: { userId: req.session.userId },
-        });
-        res.render('projects', { projects });
+        res.render('project-create');
     } catch (error) {
         console.log(error);
-        res.status(500).send('Error fetching projects');
+        res.status(400);
     }
 });
+
+module.exports = router;
